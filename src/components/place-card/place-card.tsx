a165c84bf0ea1e8503ce0ed54538795406a17e56
Mike <kononov.mixa2045@gmail.com>
2025-10-01 23:22:59 +0500

    Задание 2.13

---
+++ src/components/place-card/place-card.tsx
@@ -0,0 +1,61 @@
+type PlaceCardProps = {
+  image: string;
+  isPremium: boolean;
+  price: number;
+  title: string;
+  type: string;
+  isBookmark: boolean;
+  stars: number;
+}
+
+function PlaceCard({image, isPremium, price, title, type, isBookmark, stars}: PlaceCardProps): JSX.Element {
+  return (
+    <article className="cities__card place-card">
+      {isPremium ?
+        <div className="place-card__mark">
+          <span>Premium</span>
+        </div>
+        :
+        null}
+      <div className="cities__image-wrapper place-card__image-wrapper">
+        <a href="#">
+          <img className="place-card__image" src={image} width="260" height="200" alt="Place image"/>
+        </a>
+      </div>
+      <div className="place-card__info">
+        <div className="place-card__price-wrapper">
+          <div className="place-card__price">
+            <b className="place-card__price-value">&euro;{price}</b>
+            <span className="place-card__price-text">&#47;&nbsp;night</span>
+          </div>
+          {isBookmark ?
+            <button className="place-card__bookmark-button button" type="button">
+              <svg className="place-card__bookmark-icon" width="18" height="19">
+                <use xlinkHref="#icon-bookmark"></use>
+              </svg>
+              <span className="visually-hidden">In bookmarks</span>
+            </button>
+            :
+            <button className="place-card__bookmark-button button" type="button">
+              <svg className="place-card__bookmark-icon" width="18" height="19">
+                <use xlinkHref="#icon-bookmark"></use>
+              </svg>
+              <span className="visually-hidden">To bookmarks</span>
+            </button>}
+        </div>
+        <div className="place-card__rating rating">
+          <div className="place-card__stars rating__stars">
+            <span style={{width: `calc(100% / 5 * ${stars}`}}></span>
+            <span className="visually-hidden">Rating</span>
+          </div>
+        </div>
+        <h2 className="place-card__name">
+          <a href="#">{title}</a>
+        </h2>
+        <p className="place-card__type">{type}</p>
+      </div>
+    </article>
+  );
+}
+
+export default PlaceCard;
